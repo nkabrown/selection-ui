cda616bd73c2740ed66eb81ab9f4e9f03aed655e
Nathan Brown <nkabrown@gmail.com>
2016-06-05 14:16:52 -0400

    Initialize project
    A small example of making the choice of an item from a menu visible and
    obvious.

---
+++ selection-ui.js
@@ -0,0 +1,22 @@
+const colors = ['yellow green', 'green', 'blue green', 'blue', 'blue purple', 'purple', 'red purple', 'red', 'orange red', 'orange', 'yellow orange', 'yellow'];
+
+const colorMap = d3.scale.ordinal()
+    .domain(['yellow green', 'green', 'blue green', 'blue', 'blue purple', 'purple', 'red purple', 'red', 'orange red', 'orange', 'yellow orange', 'yellow'])
+    .range(['rgb(180,255,27)', 'rgb(34,255,3)', 'rgb(47,104,180)', 'rgb(0,0,255)', 'rgb(125,34,249)', 'rgb(142,0,239)', 'rgb(226,41,103)', 'rgb(251,0,5)', 'rgb(253,127,39)', 'rgb(253,149,10)', 'rgb(254,232,30)', 'rgb(255,255,11)']);
+
+d3.select('.selection-ui-container').selectAll('.swatch')
+    .data(colors)
+  .enter().append('div')
+    .attr('class', 'swatch')
+    .style('background-color', d => colorMap(d))
+  .append('p')
+    .attr('class', 'order-marker')
+    .text((d, i) => i);
+
+const swatches = d3.selectAll('.swatch');
+
+swatches.on('click', (d) => {
+      const selectedItem = d;
+      swatches.filter(s => s !== selectedItem).style('opacity', 0.3);
+      swatches.filter(s => s === selectedItem).style('opacity', 1);
+    });
